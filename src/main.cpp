a5182d99e5761c67cf6d2c3926cdd61441acc3e0
Allan K. Koech <allankoech.ke@gmail.com>
2024-10-13 15:41:35 +0300

    Moved the c++ code into a class

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,51 @@
-#include "app-window.h"
+#include "app_window.h"
+
+class TodoController {
+public:
+    TodoController()
+    : ui(AppWindow::create()),
+    mTodoModel(std::make_shared<slint::VectorModel<TodoData>>()) 
+    {
+        // Set the todo model as a shared VectorModel
+        ui->set_todo_model(mTodoModel);
+
+        // Handle add task callback
+        ui->on_add_task(std::bind(&TodoController::AddTask, this, std::placeholders::_1));
+
+        // Handle removing a todo item
+        ui->on_remove_task(std::bind(&TodoController::RemoveTask, this, std::placeholders::_1));
+    }
+
+    void run() {
+        // Start the event loop
+        ui->run();
+    }
+
+    bool AddTask(const slint::SharedString& task) {
+        // Push a new todo item to the vector
+        mTodoModel->push_back(TodoData(false, task));
+
+        return true;
+    }
+
+    void RemoveTask(const int& index) {
+        // Ensure passed index is less than model row_count
+        if(index >= mTodoModel->row_count()) return;
+
+        mTodoModel->erase(index);
+    }
+
+private:
+    slint::ComponentHandle<AppWindow> ui;
+
+    // Create a shared VectorModel
+    std::shared_ptr<slint::VectorModel<TodoData>> mTodoModel;
+};
 
 int main(int argc, char **argv)
 {
-    // Create & Instantiate AppWindow
-    auto ui = AppWindow::create();
-
-    // Create a shared VectorModel
-    auto model = std::make_shared<slint::VectorModel<TodoData>>();
-
-    // Set the todo model as a shared VectorModel
-    ui->set_todo_model(model);
-
-    // Handle add task callback
-    ui->on_add_task([&](const slint::SharedString& task) -> bool {
-        std::cout << "Todo: " << task << std::endl;
-
-        // Push a new todo item to the vector
-        model->push_back(TodoData(false, task));
-
-        return true;
-    });
-
-    // Handle removing a todo item
-    ui->on_remove_task([&](const int& index){
-        if(index >= model->row_count()) return;
-
-        model->erase(index);
-    });
-
-    // Start event loop
-    ui->run();
+    TodoController app;
+    app.run();
 
     return 0;
 }
